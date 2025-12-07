21af19da45574388e96e901cbf697d0b0b2e554f
arsjobus <arsjobus@outlook.com>
2025-12-07 18:18:54 +0700

    Update example-todobase.py

--- example-todobase.py
+++ example-todobase.py
@@ -2,32 +2,33 @@ from typing import List, Optional
 from pydantic import BaseModel, Field
 from enum import IntEnum
 
-# Add the Priority as an IntEnum
+# 1. Add the Priority as an IntEnum
 class Priority(IntEnum):
     HIGH = 1
     MEDIUM = 2
     LOW = 3
 
-# Add the Type: TodoBase
+# 2. Add the Type: TodoBase
 class TodoBase(BaseModel):
     todo_name: str = Field(..., min_length=3, max_length=512, description="Name of the todo")
     todo_description: str = Field(..., description="Description of the todo")
     todo_priority: Priority = Field(default=Priority.LOW, description="Priority of the todo")
 
-# Add the Type: TodoCreate
+# 3. Add the Type: TodoCreate
 class TodoCreate(TodoBase):
     pass
 
-# Add the Type: Todo
+# 4. Add the Type: Todo
 class Todo(TodoBase):
     todo_id: int = Field(..., description="Unique identifier")
 
-# Add the Type: TodoUpdate
+# 5. Add the Type: TodoUpdate
 class TodoUpdate(TodoBase):
     todo_name: Optional[str] = Field(None, min_length=3, max_length=512, description="Name of the todo")
     todo_description: Optional[str] = Field(None, description="Description of the todo")
     todo_priority: Optional[Priority] = Field(None, description="Priority of the todo")
 
+# 6. Make a fake list of todo_items
 todo_items = [
     Todo(todo_id=1, todo_name="brush teeth", todo_description="some test about the todo item", todo_priority=Priority.HIGH),
     Todo(todo_id=2, todo_name="take a shower", todo_description="some test about the todo item", todo_priority=Priority.HIGH),
