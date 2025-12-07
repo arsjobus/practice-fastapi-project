85283dc7a831573fa8b5732f5a2cb904579c6293
arsjobus <arsjobus@outlook.com>
2025-12-07 18:07:42 +0700

    Create example-todobase.py

---
+++ example-todobase.py
@@ -0,0 +1,29 @@
+from typing import List, Optional
+from pydantic import BaseModel, Field
+from enum import IntEnum
+
+# Add the Priority as an IntEnum
+class Priority(IntEnum):
+    HIGH = 1
+    MEDIUM = 2
+    LOW = 3
+
+# Add the Type: TodoBase
+class TodoBase(BaseModel):
+    todo_name: str = Field(..., min_length=3, max_length=512, description="Name of the todo")
+    todo_description: str = Field(..., description="Description of the todo")
+    priority: Priority = Field(default=Priority.LOW, description="Priority of the todo")
+
+# Add the Type: TodoCreate
+class TodoCreate(TodoBase):
+    pass
+
+# Add the Type: Todo
+class Todo(TodoBase):
+    todo_id: int = Field(..., description="Unique identifier")
+
+# Add the Type: TodoUpdate
+class TodoUpdate(TodoBase):
+    todo_name: Optional[str] = Field(None, min_length=3, max_length=512, description="Name of the todo")
+    todo_description: Optional[str] = Field(None, description="Description of the todo")
+    priority: Optional[Priority] = Field(None, description="Priority of the todo")
